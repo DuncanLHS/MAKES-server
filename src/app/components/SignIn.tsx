2766bf95cb68741429bc68e97308bc76bb14a89b
Duncan Band <band.duncan@gmail.com>
2023-05-27 13:00:01 +0100

    feat: sign in

--- src/app/components/SignIn.tsx
+++ src/app/components/SignIn.tsx
@@ -1,16 +1,19 @@
 "use client";
-import { signOut, signIn, useSession } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
+import SignInWithDiscord from "./SignInWithDiscord";
+import Button from "./ui/Button";
 
 const SignIn = () => {
   const { data } = useSession();
   return (
     <div className="flex flex-col items-center justify-center gap-4">
-      <button
-        className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-        onClick={data ? () => void signOut() : () => void signIn()}
-      >
-        {data ? "Sign out" : "Sign in"}
-      </button>
+      {data ? (
+        <Button type="button" onClick={() => void signOut()} intent="dark">
+          Sign Out
+        </Button>
+      ) : (
+        <SignInWithDiscord />
+      )}
     </div>
   );
 };
