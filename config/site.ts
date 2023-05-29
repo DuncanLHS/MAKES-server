a8185da7f8f7a78b056531051119b630198f3ff0
Duncan Band <band.duncan@gmail.com>
2023-05-29 15:15:33 +0100

    feat: init site config

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "M.A.K.E.S",
+  description: "RFID access to a Makerspace and machines",
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+  ],
+  links: {},
+};
