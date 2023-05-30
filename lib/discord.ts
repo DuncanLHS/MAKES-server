88b95b85d53f2fa3ea391b13763cbcf6ae1490a4
Duncan Band <band.duncan@gmail.com>
2023-05-30 10:42:09 +0100

    feat: role requests

--- lib/discord.ts
+++ lib/discord.ts
@@ -1,8 +1,10 @@
 import { type User } from "next-auth";
 const guildId = "1071217231515615282"; //TODO: Move guild id to db
-import type { APIGuildMember } from "discord-api-types/v10";
+import type { APIGuildMember, APIRole } from "discord-api-types/v10";
 import { type Account } from "@prisma/client";
 import { prisma } from "prisma/db";
+
+const discordApiBaseUrl = "https://discord.com/api";
 
 export const getAccount = async (user: User): Promise<Account | null> => {
   const account = await prisma.account.findFirst({
@@ -21,7 +23,7 @@ export const getMember = async (
   if (providerAccountId) {
     try {
       return await fetch(
-        `https://discord.com/api/guilds/${guildId}/members/${providerAccountId}`,
+        `${discordApiBaseUrl}/guilds/${guildId}/members/${providerAccountId}`,
         {
           headers: {
             Authorization: `Bot ${process.env.DISCORD_BOT_TOKEN as string}`,
@@ -38,4 +40,35 @@ export const getMember = async (
   } else {
     return undefined;
   }
+};
+
+export const getServerRoles = async () => {
+  //https://discord.com/developers/docs/topics/permissions#role-object
+  //https://discord.com/developers/docs/resources/guild#get-guild-roles
+
+  try {
+    return await fetch(`${discordApiBaseUrl}/guilds/${guildId}/roles`, {
+      headers: {
+        Authorization: `Bot ${process.env.DISCORD_BOT_TOKEN as string}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data: APIRole[]) => {
+        return data;
+      });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const getRoleDetails = async (
+  roleIds: string[]
+): Promise<APIRole[] | undefined> => {
+  //https://discord.com/developers/docs/topics/permissions#role-object
+  //https://discord.com/developers/docs/resources/guild#get-guild-role
+
+  const serverRoles = await getServerRoles();
+  if (!serverRoles) return undefined;
+  const roles = serverRoles.filter((role) => roleIds.includes(role.id));
+  return roles;
 };
