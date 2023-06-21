070b500eb1e3d5cf8ecdd6c52b9a9562aa97baa5
Duncan Band <band.duncan@gmail.com>
2023-06-21 23:57:10 +0100

    fix: rm unused ts escape hatch

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -4,7 +4,6 @@ import User from "../../components/User";
 const page = () => {
   return (
     <main className="prose flex h-screen max-w-full flex-grow flex-col items-center justify-center">
-      {/* @ts-expect-error server component */}
       <User />
     </main>
   );
